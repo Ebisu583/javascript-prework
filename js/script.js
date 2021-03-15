5f41add50290f19532e4ca1fdef7c5ea49a1389e
Radoslaw Tyszkiewicz <ebisu.poland@gmail.com>
2021-03-15 18:05:10 +0900

    Added JS code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = `papier`;
+
+printMessage(`Zagralem ` + playerMove + `! Jesli Twoj ruch to nozyce, to wygrywasz!`);
