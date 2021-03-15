bb9a68bf22d6cb672d57edbb807f1522334c46b1
Radoslaw Tyszkiewicz <ebisu.poland@gmail.com>
2021-03-15 19:11:39 +0900

    Add player move

--- js/script.js
+++ js/script.js
@@ -10,3 +10,15 @@ else if ( randomNumber == 3 ) {
     computerMove = `scissors`;
 }
 console.log(computerMove);
+let playerAnswer = prompt( `Choose a number 1-stone 2-scissors 3-paper` );
+let playerMove = ``;
+if ( playerAnswer == 1 ) {
+    playerrMove = `stone`;
+}
+else if ( playerAnswer == 2 ) {
+    playerMove = `paper`;
+}
+else if ( playerAnswer == 3 ) {
+    playerMove = `scissors`;
+}
+console.log(playerMove);
