3f65c3a41b819b6ed9e1d7cc90e9d02a8cd1048f
Radoslaw Tyszkiewicz <ebisu.poland@gmail.com>
2021-03-24 19:19:19 +0900

    Created interface

--- js/functions.js
+++ js/functions.js
@@ -38,3 +38,32 @@ function displayResult(argComputerMove, argPlayerMove) {
 		printMessage('You lose! Computer move: ' + argComputerMove + `- Player move: ` + argPlayerMove);  
 	}
 }
+function playGame(playerNumber) {
+	clearMessages();
+	let randomNumber = Math.floor(Math.random()*3+1);
+	let computerMove = getMoveName(randomNumber);
+	// if ( randomNumber == 1 ) {
+	//     computerMove = `stone`;
+	// }
+	// else if ( randomNumber == 2 ) {
+	//     computerMove = `paper`;
+	// }
+	// else if ( randomNumber == 3 ) {
+	//     computerMove = `scissors`;
+	// }
+	// console.log(computerMove);
+	// let playerAnswer = prompt( `Choose a number 1-stone 2-paper 3-scissors` );
+	let playerMove = getMoveName(playerNumber);
+	// if ( playerAnswer == 1 ) {
+	//     playerMove = `stone`;
+	// }
+	// else if ( playerAnswer == 2 ) {
+	//     playerMove = `paper`;
+	// }
+	// else if ( playerAnswer == 3 ) {
+	//     playerMove = `scissors`;
+	// }
+	// console.log(playerMove);
+	displayResult(computerMove, playerMove)
+
+}
--- js/script.js
+++ js/script.js
@@ -1,25 +1,13 @@
-let randomNumber = Math.floor(Math.random()*3+1);
-let computerMove = getMoveName(randomNumber);
-// if ( randomNumber == 1 ) {
-//     computerMove = `stone`;
-// }
-// else if ( randomNumber == 2 ) {
-//     computerMove = `paper`;
-// }
-// else if ( randomNumber == 3 ) {
-//     computerMove = `scissors`;
-// }
-// console.log(computerMove);
-let playerAnswer = prompt( `Choose a number 1-stone 2-paper 3-scissors` );
-let playerMove = getMoveName(playerAnswer);
-// if ( playerAnswer == 1 ) {
-//     playerMove = `stone`;
-// }
-// else if ( playerAnswer == 2 ) {
-//     playerMove = `paper`;
-// }
-// else if ( playerAnswer == 3 ) {
-//     playerMove = `scissors`;
-// }
-// console.log(playerMove);
-displayResult(computerMove, playerMove)
+// playGame(3);
+const rock = document.getElementById("play-rock");
+const paper = document.getElementById("play-paper");
+const scissors = document.getElementById("play-scissors")
+rock.addEventListener(`click`,function(){
+    playGame(1);
+})
+paper.addEventListener(`click`,function(){
+    playGame(2);
+})
+scissors.addEventListener(`click`,function(){
+    playGame(3);
+})
