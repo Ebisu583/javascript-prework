53afb4ca794170e2cd462a82d1beef7ff5b23d56
Radoslaw Tyszkiewicz <ebisu.poland@gmail.com>
2021-03-24 18:29:30 +0900

    Created functions

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,34 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+function getMoveName(choice) {
+	if ( choice == 1 ) {
+		return `stone`;
+	}
+	else if ( choice == 2 ) {
+		return `paper`;
+	}
+	else if ( choice == 3 ) {
+		return `scissors`;
+	}
+}
+function displayResult(argComputerMove, argPlayerMove) {
+	if (argPlayerMove == ``) {
+		printMessage(`Unknown move`);
+	}
+	else if (argComputerMove == argPlayerMove) {
+		printMessage('It is a draw Computer move: ' + argComputerMove + `- Player move: ` + argPlayerMove);
+	}
+	else if( argComputerMove == 'stone' && argPlayerMove == 'paper'){
+		printMessage('You win! Computer move: ' + argComputerMove + `- Player move: ` + argPlayerMove);
+	} 
+	else if( argComputerMove == 'paper' && argPlayerMove == 'scissors'){
+		printMessage('You win! Computer move: ' + argComputerMove + `- Player move: ` + argPlayerMove);
+	} 
+	else if( argComputerMove == 'scissors' && argPlayerMove == 'stone'){
+		printMessage('You win! Computer move: ' + argComputerMove + `- Player move: ` + argPlayerMove);
+	} 
+	else {
+		printMessage('You lose! Computer move: ' + argComputerMove + `- Player move: ` + argPlayerMove);  
+	}
+}
--- js/script.js
+++ js/script.js
@@ -1,42 +1,25 @@
 let randomNumber = Math.floor(Math.random()*3+1);
-let computerMove = ``;
-if ( randomNumber == 1 ) {
-    computerMove = `stone`;
-}
-else if ( randomNumber == 2 ) {
-    computerMove = `paper`;
-}
-else if ( randomNumber == 3 ) {
-    computerMove = `scissors`;
-}
-console.log(computerMove);
-let playerAnswer = prompt( `Choose a number 1-stone 2-scissors 3-paper` );
-let playerMove = ``;
-if ( playerAnswer == 1 ) {
-    playerMove = `stone`;
-}
-else if ( playerAnswer == 2 ) {
-    playerMove = `paper`;
-}
-else if ( playerAnswer == 3 ) {
-    playerMove = `scissors`;
-}
-console.log(playerMove);
-if (playerMove == ``) {
-    printMessage(`Unknown move`);
-}
-else if (computerMove == playerMove) {
-    printMessage('It is a draw Computer move: ' + computerMove + `- Player move: ` + playerMove);
-}
-else if( computerMove == 'stone' && playerMove == 'paper'){
-    printMessage('You win! Computer move: ' + computerMove + `- Player move: ` + playerMove);
-} 
-else if( computerMove == 'paper' && playerMove == 'scissors'){
-    printMessage('You win! Computer move: ' + computerMove + `- Player move: ` + playerMove);
-} 
-else if( computerMove == 'scissors' && playerMove == 'stone'){
-    printMessage('You win! Computer move: ' + computerMove + `- Player move: ` + playerMove);
-} 
-else {
-    printMessage('You lose! Computer move: ' + computerMove + `- Player move: ` + playerMove);  
-}
+let computerMove = getMoveName(randomNumber);
+// if ( randomNumber == 1 ) {
+//     computerMove = `stone`;
+// }
+// else if ( randomNumber == 2 ) {
+//     computerMove = `paper`;
+// }
+// else if ( randomNumber == 3 ) {
+//     computerMove = `scissors`;
+// }
+// console.log(computerMove);
+let playerAnswer = prompt( `Choose a number 1-stone 2-paper 3-scissors` );
+let playerMove = getMoveName(playerAnswer);
+// if ( playerAnswer == 1 ) {
+//     playerMove = `stone`;
+// }
+// else if ( playerAnswer == 2 ) {
+//     playerMove = `paper`;
+// }
+// else if ( playerAnswer == 3 ) {
+//     playerMove = `scissors`;
+// }
+// console.log(playerMove);
+displayResult(computerMove, playerMove)
